1f0ed1a6c45782ad9caf7ebf67bacf777a512b16
Coder Sparks <codersparks@gmail.com>
2018-12-19 20:21:27 +0000

    Committing to convert to using ValidationAttribute

---
+++ GrandadAudioPlayerAdmin/Utils/AllowedExtensionValidationRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace GrandadAudioPlayerAdmin.Utils
+{
+    class AllowedExtensionValidationRule : ValidationRule
+    {
+        private static readonly Regex rx = new Regex(@"^\.[A-Za-z0-9]+(?:,\.[A-Za-z0-9]+)*$", RegexOptions.Compiled);
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return new ValidationResult(value is string testValue && rx.IsMatch(testValue), "Does not match comma separated list of extensions ");
+        }
+    }
+}
