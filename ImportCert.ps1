c6bada98eabe373961bc30d8df49e99da149c005
Coder Sparks <codersparks@gmail.com>
2018-12-23 18:06:26 +0000

    Still trying

--- ImportCert.ps1
+++ ImportCert.ps1
@@ -2,6 +2,7 @@ $pfxpath = 'GrandadAudioPlayer\GrandadAudioPlayer_TemporaryKey.pfx'
 $password = 'letmein'
 
 dir
+dir GrandadAudioPlayer
 
 Add-Type -AssemblyName System.Security
 $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
