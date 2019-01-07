92ba33bcf409e8e2718fbef3afe412d2c95c3c15
Coder Sparks <codersparks@gmail.com>
2019-01-07 19:55:24 +0000

    Updated to get squirrel to run

---
+++ RunSquirrel.ps1
@@ -0,0 +1,5 @@
+$nupkgs = @(gci .\GrandadAudioPlayer\ -Recurse -Filter GrandadAudioPlayer*.nupkg | ?{ !$_.PSIsContainer } | % { $_.FullName })
+
+if ($nupkgs.length -ne 1) { Write-Warning "More than one nupkg found using first one: $npkgs[0]"}
+
+.\packages\squirrel.windows.1.9.0\tools\Squirrel.exe --no-msi --no-delta --releasify $nupkgs[0]
