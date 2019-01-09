adcf1d3d76fa0dadd2741c20c047dcfd1fd687cd
Coder Sparks <codersparks@gmail.com>
2019-01-09 20:02:46 +0000

    Added power button to titleview

--- GrandadAudioPlayer/ViewModels/TitleViewModel.cs
+++ GrandadAudioPlayer/ViewModels/TitleViewModel.cs
@@ -1,10 +1,27 @@
-﻿using GrandadAudioPlayer.Utils.Playlist;
+﻿using System.Diagnostics;
+using GrandadAudioPlayer.Utils.Playlist;
+using log4net;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace GrandadAudioPlayer.ViewModels
 {
     public class TitleViewModel : BindableBase
     {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(TitleViewModel));
 
+        public TitleViewModel()
+        {
+            PowerCommand = new DelegateCommand(PowerMethod);
+        }
+
+        public DelegateCommand PowerCommand { get; }
+
+        public void PowerMethod()
+        {
+            Logger.Info("User pressed 'Turn Off' button, shutting down computer");
+            // Process.Start("shutdown.exe", "-s -t 00");
+            Logger.Error("**************** Shutdown command has been disabled ****************");
+        }
     }
 }
